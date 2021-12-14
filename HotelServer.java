96f5504ffa99869adc71b2b0e9638a29a6768e02
ndyabagye <ndyabagyehenrytusi@gmail.com>
2021-12-14 14:02:34 +0300

    refactor in the hotel server

--- HotelServer.java
+++ HotelServer.java
@@ -12,16 +12,15 @@ public class HotelServer{
 
 			//It binds (registers) to the rmregistry
 			Naming.rebind("rmi://localhost:5000/HotelService", r);
-			
+
 			//It binds (registers) to the rmregistry
-			
 		}
 		catch (Exception e){
 			System.out.println("Trouble: _" + e);
-		}	
+		}
 	}
 
 	public static void main (String args []){
-		new HotelServer();	
+		new HotelServer();
 	}
 }
